04f25b83de127eb7aec5a01f9729205ee49910ab
jacobabr9 <108356402+jacobabr9@users.noreply.github.com>
2025-03-23 10:29:00 -0400

    Update student-login.php

--- mind/student-login.php
+++ mind/student-login.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 
 <?php
-session_start();
 // Database credentials
-$host = "localhost"; // If your database is on the same server as your website
-$username = "root";   // Change to your MySQL username
-$password = "";        // Change to your MySQL password
-$dbname = "djkyd";    // The name of your database
+$host = "djkyd-ai-support.site"; 
+$username = "root";   
+$password = "";        
+$dbname = "djkyd";   
 
 // Create connection
 $conn = new mysqli($host, $username, $password, $dbname);
