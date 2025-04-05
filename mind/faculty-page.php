1e7a39e9e4142fb75b887f70df454d2515566b70
David Almeida Da Graca <160807004+daviddagraca@users.noreply.github.com>
2025-04-04 22:02:07 -0400

    Fix header?

--- mind/faculty-page.php
+++ mind/faculty-page.php
@@ -52,46 +52,46 @@ session_start();
 <body class="main-layout">
 
 <!-- header -->
-  <header>
-    <!-- header inner -->
-    <div class="header-top">
-      <div class="header">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-xl-2 col-lg-4 col-md-4 col-sm-3 col logo_section">
-              <div class="full">
-                <div class="center-desk">
-                  <div class="logo">
-                    <a href="index.php"><img src="images/colorized.png" alt="#" /></a>
-                  </div>
+<header>
+  <div class="header-top">
+    <div class="header">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-xl-2 col-lg-4 col-md-4 col-sm-3 col logo_section">
+            <div class="full">
+              <div class="center-desk">
+                <div class="logo">
+                  <a href="index.php"><img src="images/colorized.png" alt="Logo" /></a>
                 </div>
               </div>
             </div>
-            <div class="col-xl-10 col-lg-8 col-md-8 col-sm-9">
-              <div class="header_information">
-               <div class="menu-area">
+          </div>
+          <div class="col-xl-10 col-lg-8 col-md-8 col-sm-9">
+            <div class="header_information">
+              <div class="menu-area">
                 <div class="limit-box">
-                  <nav class="main-menu ">
+                  <nav class="main-menu">
                     <ul class="menu-area-main">
-                      <li> <a href="index.php">Home</a> </li>
-                      <li> <a href="students-page.php">Students</a> </li>
-                      <li class="active"> <a href="#about">Faculty</a> </li>
-                      <li> <a href="#learn">Community</a> </li>
-                      <li> <a href="#important">Ask AI</a> </li>
-                      <li> <a href="#contact">News</a> </li>
-                     </ul>
-                   </nav>
-                 </div>
-               </div> 
-               <div class="mean-last">           
-             </div>
-           </div>
-         </div>
-       </div>
-     </div>
-     <!-- end header inner -->
-
-     <!-- end header -->
+                      <li><a href="index.php">Home</a></li>
+                      <li class="active"><a href="#courses">Students</a></li>
+                      <li><a href="faculty-page.php">Faculty</a></li>
+                      <li><a href="#learn">Community</a></li>
+                      <li><a href="#important">Ask AI</a></li>
+                      <li><a href="#contact">News</a></li>
+                    </ul>
+                  </nav>
+                </div>
+              </div>
+              <div class="mean-last">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+	
      <section class="slider_section">
       <div id="myCarousel" class="carousel slide" data-ride="carousel">
         <!--<ol class="carousel-indicators">
