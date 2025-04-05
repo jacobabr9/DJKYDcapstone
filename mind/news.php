4651dbc2a91a003ef66f3d872c6fd3fdbc00cc45
David Almeida Da Graca <160807004+daviddagraca@users.noreply.github.com>
2025-04-04 22:11:37 -0400

    Fixed top bar

--- mind/news.php
+++ mind/news.php
@@ -54,7 +54,7 @@
                     <nav class="main-menu">
                       <ul class="menu-area-main">
                       <li><a href="index.php">Home</a></li>
-                      <li class="active"><a href="#courses">Students</a></li>
+                      <li><a href="#courses">Students</a></li>
                       <li><a href="faculty-page.php">Faculty</a></li>
                       <li><a href="#learn">Community</a></li>
                       <li><a href="#important">Ask AI</a></li>
