c6b6b1a66b842e37c3268b0c79f768fb7921d98a
David Almeida Da Graca <160807004+daviddagraca@users.noreply.github.com>
2025-04-08 19:06:50 -0400

    Update about-us.php

--- mind/about-us.php
+++ mind/about-us.php
@@ -540,36 +540,5 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'student') {
           <script src="js/custom.js"></script>
           <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
 
-
-          <script>
-// This example adds a marker to indicate the position of Bondi Beach in Sydney,
-// Australia.
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 11,
-    center: {
-      lat: 40.645037,
-      lng: -73.880224
-    },
-  });
-
-  var image = 'images/maps-and-flags.png';
-  var beachMarker = new google.maps.Marker({
-    position: {
-      lat: 40.645037,
-      lng: -73.880224
-    },
-    map: map,
-    icon: image
-  });
-}
-</script>
-<!-- google map js -->
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8eaHt9Dh5H57Zh0xVTqxVdBFCvFMqFjQ&callback=initMap"></script>
-<!-- end google map js -->
-
-
-
 </body>
-
 </html>
